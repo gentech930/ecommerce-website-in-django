a937dcad5eade07c361f7c52f92351dd867e169a
Shaista Siddique <150334826+gentech930@users.noreply.github.com>
2024-09-07 15:31:46 +0500

    models.py

--- app/models.py
+++ app/models.py
@@ -13,5 +13,14 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+class Cart(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
 
 # Create your models here.
