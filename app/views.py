2071340cf936e2246035499ea2761f1cfb6e77c4
Shaista Siddique <150334826+gentech930@users.noreply.github.com>
2024-09-07 15:30:19 +0500

    views.py

--- app/views.py
+++ app/views.py
@@ -6,6 +6,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product
 from django.shortcuts import render, get_object_or_404
 from .models import Product
+from django.contrib.auth.decorators import login_required
+from .models import Product, Cart, CartItem
 
 def product_detail(request, product_id):
     product = get_object_or_404(Product, id=product_id)
@@ -26,6 +28,39 @@ def cart(request):
     return render(request, 'app/cart.html')
 def checkout(request):
     return render(request, 'app/checkout.html')
+
+
+@login_required
+def add_to_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    cart, created = Cart.objects.get_or_create(user=request.user)
+    cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+    if not created:
+        cart_item.quantity += 1
+        cart_item.save()
+    return redirect('cart_view')
+
+@login_required
+def cart_view(request):
+    cart, created = Cart.objects.get_or_create(user=request.user)
+    items = cart.items.all()
+    return render(request, 'cart.html', {'cart': cart, 'items': items})
+from django.shortcuts import render, redirect
+
+def add_to_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    cart = request.session.get('cart', {})
+    if product_id in cart:
+        cart[product_id] += 1
+    else:
+        cart[product_id] = 1
+    request.session['cart'] = cart
+    return redirect('cart_view')
+
+def cart_view(request):
+    cart = request.session.get('cart', {})
+    products = Product.objects.filter(id__in=cart.keys())
+    return render(request, 'app/cart.html', {'cart': cart, 'products': products})
 
 
 
